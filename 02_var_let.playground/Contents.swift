769867160647ff6e02fd07ed0b772e20271ff6f3
Jose Ricardo Mendoza Villar <richi_1325@hotmail.com>
2022-03-10 14:44:52 -0600

    feature: Adding 2 playground

---
+++ 02_var_let.playground/Contents.swift
@@ -0,0 +1,34 @@
+import UIKit
+
+/*
+By: Jose Ricardo Mendoza Villar
+Date: 03/10/2022
+*/
+
+let maximumNumberOfLoginAttempts = 3
+
+var currentLoginAttempt = 0
+
+var x = 0.0, y = 0.0, z = 0.0
+
+// ---- Type Annotations ----
+var welcomeMessage : String
+welcomeMessage = "Hola ¿Qué tal?"
+
+var red, green, blue : Int64
+
+// ---- Special names for varibles ----
+let π = 3.141592
+let 👋🏽 = "Hola"
+print(👋🏽)
+
+// ---- Using Print ----
+// ---- Interpolación ----
+print("El número de login actual es: \(currentLoginAttempt) de un total de \(maximumNumberOfLoginAttempts)")
+print("Hola", "Soy Ricardo","Es un gusto",separator: ", ", terminator: ".")
+
+/*
+ Comentarios anidados
+ /* Esto permite multicomentarios*/
+ Esto esta genial 🤯
+*/
